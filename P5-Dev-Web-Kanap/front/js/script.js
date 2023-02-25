be9f95844fa01c454d2836baa534396a1fbde207
Cyril Derc <cyril_derc@hotmail.com>
2023-02-25 17:01:04 +0100

    création fonction pour infos API

---
+++ P5-Dev-Web-Kanap/front/js/script.js
@@ -0,0 +1,25 @@
+const url = "http://localhost:3000/api/products"
+const container = document.getElementById('items')
+
+
+//création de la fonction récupérant les infos de l'API
+const getArticles = () => {
+    fetch(url)
+    .then(function(res) {
+            return res.json()
+    })
+    .then(function(data) {
+        console.log(data);
+        for(product in data) {
+            container.innerHTML += `<a href="./product.html?id=42">
+            <article>
+              <img src="${data[product].imageUrl}" alt="${data[product].altTxt}">
+              <h3 class="productName">${data[product].name}</h3>
+              <p class="productDescription">${data[product].description}</p>
+            </article>
+          </a>`
+        }
+    })
+}
+
+getArticles()
