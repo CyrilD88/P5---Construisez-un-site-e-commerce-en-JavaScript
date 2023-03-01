a008dfded966d8b334f0ef1c6aeeac474a941318
Cyril Derc <cyril_derc@hotmail.com>
2023-03-01 22:39:43 +0100

    injection des données product.js

---
+++ P5-Dev-Web-Kanap/front/js/product.js
@@ -0,0 +1,28 @@
+const params = new URL(document.location).searchParams
+const id = params.get("id")
+
+const url = `http://localhost:3000/api/products/${id}`
+
+
+
+////création de la fonction récupérant les infos de l'API
+const getArticle = () => {
+    fetch(url)
+    .then(function(res) {
+            return res.json()
+    })
+    .then(function(data){
+        console.log(data)
+        const addTitle = (document.getElementById("title").innerHTML=data.name)  //création du titre du canapé
+        const addPrice = (document.getElementById("price").innerHTML=data.price) //création du prix du canapé
+        const addImg = document.createElement("img")  //création de l'élément image
+        document.querySelector(".item__img").appendChild(addImg) // intégration de l'élément image à la div parente
+        addImg.setAttribute("src", `${data.imageUrl}`) //ajout de l'image par son url
+
+        const addDescription = (document.getElementById("description").innerHTML=data.description) //création de la description du canapé
+
+
+    })
+}
+
+getArticle()
--- P5-Dev-Web-Kanap/front/js/script.js
+++ P5-Dev-Web-Kanap/front/js/script.js
@@ -11,7 +11,7 @@ const getArticles = () => {
     .then(function(data) {
         console.log(data);
         for(product in data) {
-            container.innerHTML += `<a href="./product.html?id=42">
+            container.innerHTML += `<a href="./product.html?id=${data[product]._id}">
             <article>
               <img src="${data[product].imageUrl}" alt="${data[product].altTxt}">
               <h3 class="productName">${data[product].name}</h3>
