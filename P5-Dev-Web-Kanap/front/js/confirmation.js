7de161bb02fc67ff5f7907a9ff37c5eab699cdad
Cyril Derc <cyril_derc@hotmail.com>
2023-03-09 15:23:36 +0100

    modification conformation

---
+++ P5-Dev-Web-Kanap/front/js/confirmation.js
@@ -0,0 +1,8 @@
+
+//Création d'un fonction nettoyant le localstorage et donnant l'orderId
+function confirmation() {
+    let numeroConfirmation = document.getElementById("orderId");
+    numeroConfirmation.innerText = localStorage.getItem("orderId");
+    localStorage.clear();
+}
+confirmation();
--- P5-Dev-Web-Kanap/front/js/product.js
+++ P5-Dev-Web-Kanap/front/js/product.js
@@ -1,79 +1,109 @@
-const params = new URL(document.location).searchParams
-const id = params.get("id")
+//Récupération de l'ID présent dans l'URL
+var url = new URL(document.location).searchParams;
+var urlID = url.get("id");
+console.log(urlID);
 
-const url = `http://localhost:3000/api/products/${id}`
+//Création des constantes pour l'intégration dynamique dans le DOM
+const Image = document.querySelector(".item__img");
+const titre = document.getElementById('title');
+const prix = document.getElementById('price');
+const description = document.getElementById('description');
+const couleur = document.getElementById('colors');
 
-
-
-////création de la fonction récupérant les infos de l'API
-const getArticle = () => {
-    fetch(url)
+//Récupération des informations de l'article dans la base de donnée
+function ficheArticles() {
+fetch("http://localhost:3000/api/products/" + urlID)
     .then(function(res) {
-            return res.json()
+        if (res.ok) {
+            return res.json();
+        }
     })
-    .then(function(data){
-        // console.log(data)
-        const addTitle = (document.getElementById("title").innerHTML=data.name)  //création du titre du canapé
-        const addPrice = (document.getElementById("price").innerHTML=data.price) //création du prix du canapé
-        const addImg = document.createElement("img")  //création de l'élément image
-        document.querySelector(".item__img").appendChild(addImg) // intégration de l'élément image à la div parente
-        addImg.setAttribute("src", `${data.imageUrl}`) //ajout de l'image par son url
+    .then(function(data) {
+        let produit = data;
+        console.log(data);
 
-        const addDescription = (document.getElementById("description").innerHTML=data.description) //création de la description du canapé
-        const addOption = document.getElementById("colors") //création des choix de couleur du canapé
-        for (dyeing in data.colors) {
-            addOption.innerHTML += `<option value= "${data.colors[dyeing]}">${data.colors[dyeing]}</option>`
+        //Création du bloc DOM image et alt
+        let imageProduit = document.createElement("img");
+        Image.appendChild(imageProduit);
+        imageProduit.src = produit.imageUrl;
+        imageProduit.alt = produit.altTxt;
+
+        //Insertion dans le DOM du titre, prix et description
+        titre.innerText = produit.name;
+        prix.innerText = produit.price;
+        description.innerText = produit.description;
+        
+        /*Pour chaque  couleur existante dans la base de donnée :
+        *Création de la balise option
+        *Insertion de la couleur 
+        */ 
+        for (let i in produit.colors) {
+            let optionProduits = document.createElement("option");
+            couleur.appendChild(optionProduits);
+            optionProduits.innerText = produit.colors[i];
         }
 
     })
+    .catch(function(err) {
+        let erreur = document.querySelector("item__img");
+        erreur.innerText = "Une erreur n'a pas permis d'afficher notre canapé. Veuillez nous excuser pour ce désagrement et nous vous invitons a réessayer ultérieurement. <br> L'équipe Kanap."
+        console.log("Erreur dans la récupération de l'article");
+    });
 }
 
-getArticle()
+ficheArticles();
 
-//enregistrement des choix utilisateurs
-const button = document.getElementById("addToCart");
-const mistake = document.createElement("div");
-document.querySelector(".item__content__addButton").appendChild(mistake);
-const addprodLocalStorage = [];
+//Création des constantes pour l'ajout des articles dans le panier
+const BtnPanier = document.getElementById("addToCart");
+const erreur = document.createElement("div");
+document.querySelector(".item__content__addButton").appendChild(erreur);
+var tableauProduit = [];
 
-//Création "ajouter au panier"
-button.addEventListener('click', function() {
-    mistake.innerText = "";
+//Création de l'écoute du bouton "ajouter au panier"
+BtnPanier.addEventListener('click', function() {
+    erreur.innerText = "";
+    /*Si: 
+    la quantité est supérieur à 0 ET
+    la quantité est inférieur ou égale à 100 ET
+    une couleur est selectionnée
+    alors on peut ajouter un nouveau produit au panier*/
     if ((document.getElementById("quantity").value > 0 && document.getElementById("quantity").value <= 100) && (document.querySelector("#colors").value !== "")){
-        let newProduct = {
-            id: id,
+        /*nouveau produit 
+        _id: string
+        quantity: string
+        colors: string */
+        let nouveauProduit = {
+            _id: urlID,
             quantity: document.getElementById("quantity").value,
-            colors: document.getElementById("colors").value
+            colors: couleur.value
 
         };
 
-        //S'il y a déjà un article dans le panier, ==> ajouter un nouveau produits à la liste
+        //S'il y a déjà un article dans le panier, on le met en forme JS pour pouvoir ajouter un nouveau produits à la liste
         if (localStorage.getItem('produits') !== null) {
-            addprodLocalStorage = JSON.parse(localStorage.getItem('produits'));
+            tableauProduit = JSON.parse(localStorage.getItem('produits'));
         }
         
-        //on peut ajuster la quantité si un nouveau produit à le même ID ET la même couleur qu'un produit déjà existant dans le localstorage
-        for (let doublon of addprodLocalStorage) {
-            if (doublon._id === newProduct._id && doublon.colors === newProduct.colors) {
-                let addition = parseInt(doublon.quantity) + parseInt(newProduct.quantity);
+        //Si un nouveau produit à le même ID ET la même couleur qu'un produit déjà existant dans le localstorage, alors on ajuste uniquement la quantité
+        for (let doublon of tableauProduit) {
+            if (doublon._id === nouveauProduit._id && doublon.colors === nouveauProduit.colors) {
+                let addition = parseInt(doublon.quantity) + parseInt(nouveauProduit.quantity);
                 if (addition > 100) {
                     addition = 100;
                 } else if (addition < 0) {
                     addition = 0;
                 }
                 doublon.quantity = JSON.stringify(addition);
-                return (localStorage.setItem('produits', JSON.stringify(addprodLocalStorage)));
+                return (localStorage.setItem('produits', JSON.stringify(tableauProduit)));
             }
         }
 
-        // ajouter un nouveau produit au panier 
-        addprodLocalStorage.push(newProduct);
-        localStorage.setItem("produits", JSON.stringify(addprodLocalStorage));
+        //On ajoute le nouveau produit aux panier et on transforme le localstorage en JSON pour pouvoir l'exporter dans la page panier
+        tableauProduit.push(nouveauProduit);
+        localStorage.setItem("produits", JSON.stringify(tableauProduit));
         alert("Le produit à bien été ajouté au panier.");
     } else {
-        mistake.innerText = "Veuillez renseigner une quantité (entre 1 et 100) et choisir une couleur de canapé";
+        erreur.innerText = "Veuillez renseigner une quantité (entre 1 et 100) et choisir une couleur de canapé";
 
     };
 });
-
-
